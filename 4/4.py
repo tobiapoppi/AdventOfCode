b875682dd75b5051486a3e5fa22beb3b7e3031af
Tobia Poppi <tobia.poppi@virgilio.it>
2023-12-04 06:45:00 +0100

    day 4 sol

---
+++ 4/4.py
@@ -0,0 +1,35 @@
+with open('4/input.txt', 'r') as f:
+    cards = f.read().split('\n')
+
+def calculate_points(cards):
+    total_points = 0
+
+    for card in cards:
+        winning_numbers, your_numbers = card.split(' | ')
+        winning_numbers = winning_numbers.split(':')[1]
+        winning_numbers = set(map(int, winning_numbers.split()))
+        your_numbers = set(map(int, your_numbers.split()))
+
+        matches = winning_numbers.intersection(your_numbers)
+        if matches:
+            total_points += 2 ** (len(matches) - 1)  # Double points for each match after the first
+        else:
+            pass
+    
+    n = len(cards)
+    card_instances = [1] * n
+
+    for i in range(n):
+        winning_numbers, your_numbers = cards[i].split(' | ')
+        winning_numbers = winning_numbers.split(':')[1]
+        winning_numbers = set(map(int, winning_numbers.split()))
+        your_numbers = set(map(int, your_numbers.split()))
+
+        nmatches = len(winning_numbers.intersection(your_numbers))
+        for j in range(i+1, min(i+1+nmatches, n)):
+            card_instances[j] += card_instances[i]
+
+    return total_points, sum(card_instances)
+
+print(calculate_points(cards))
+
